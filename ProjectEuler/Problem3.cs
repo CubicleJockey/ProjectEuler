0cb62daa5f629f5b2e73b970c5e73f0e99e515f9
André Davis <CubicleJockey@users.noreply.github.com>
2021-02-11 22:00:46 -0800

    Helping others with homework. Break

---
+++ ProjectEuler/Problem3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectEuler
+{
+    /// <summary>
+    /// Largest prime factor 
+    /// Problem 3
+    /// 
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// 
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// </summary>
+    [TestClass]
+    public class Problem3
+    {
+        [TestMethod]
+        public void Solution()
+        {
+
+        }
+    }
+}
